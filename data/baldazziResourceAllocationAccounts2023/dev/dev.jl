a22237fc9c78127b3625d95a6b91510edbc989e2
josePereiro <jose.pereiro.personal@gmail.com>
2025-01-03 02:21:23 -0500

    Add raw.elife-79815-supp1-v2

---
+++ data/baldazziResourceAllocationAccounts2023/dev/dev.jl
@@ -0,0 +1,30 @@
+@time begin
+    using JSON
+    using CSV
+    using DataFrames
+end
+
+let
+    csvdir = joinpath(@__DIR__, "proc")
+    
+    csvfile = joinpath(csvdir, "Batch-elife-79815-supp1-v2.csv")
+    # csvfile = joinpath(csvdir, "Batch-elife-79815-supp2-v2.csv")
+    # csvfile = joinpath(csvdir, "Chemostat-elife-79815-supp1-v2.csv")
+    
+    
+    df = CSV.read(csvfile, DataFrame; delim = ',', stringtype=String)
+    
+    dict = Dict()
+    for colname in names(df)
+        dict[colname] = Dict(
+            "data" => df[:, colname]
+        )
+    end
+    
+    jsonfile = replace(csvfile, ".csv" => ".json")
+    @show jsonfile
+    open(jsonfile, "w") do io
+        println(io, JSON.json(dict))
+    end
+
+end
